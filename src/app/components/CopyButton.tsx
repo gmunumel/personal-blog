43ac83e61d5ff19792f241b0f80f54211d7cb8e9
Gabriel Munumel <gabrielmunumel@gmail.com>
2025-06-18 19:41:29 +0200

    feat: minor updated in copy button and added github repository

--- src/app/components/CopyButton.tsx
+++ src/app/components/CopyButton.tsx
@@ -13,7 +13,7 @@ export default function CopyButton({ getText }: { getText: () => string }) {
   return (
     <button
       onClick={handleCopy}
-      className="absolute top-2 right-2 z-10 px-2 py-1 text-xs rounded bg-blue-600 text-white opacity-80 hover:opacity-100 transition cursor-pointer"
+      className="absolute top-2 mt-2 mr-2 right-2 z-10 px-2 py-1 text-xs rounded bg-blue-600 text-white opacity-80 hover:opacity-100 transition cursor-pointer"
       aria-label="Copy code"
       type="button"
     >
